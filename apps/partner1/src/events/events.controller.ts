7ac1fde4e88ce94e2a63761c87c8b7cdf8b0edb0
vitorcolazelli <vitorcolazelli@gmail.com>
2024-07-15 22:00:22 -0300

    first commit

---
+++ apps/partner1/src/events/events.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards } from '@nestjs/common';
+import { EventsService } from '@app/core/events/events.service';
+import { CreateEventRequest } from './request/create-event.request';
+import { UpdateEventRequest } from './request/update-event.request';
+import { ReserveSportRequest } from './request/reserve-spot.request';
+import { AuthGuard } from '@app/core/auth/auth.guard';
+
+@Controller('events')
+export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Post()
+  create(@Body() createEventDto: CreateEventRequest) {
+    return this.eventsService.create(createEventDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.eventsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.eventsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateEventDto: UpdateEventRequest) {
+    return this.eventsService.update(id, updateEventDto);
+  }
+
+  @HttpCode(204)
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.eventsService.remove(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post(':id/reserve')
+  reserveSpots(@Body() reserveRequest: ReserveSportRequest, @Param('id') eventId: string){
+    return this.eventsService.reserveSpot({ ...reserveRequest, eventId });
+  }
+}
